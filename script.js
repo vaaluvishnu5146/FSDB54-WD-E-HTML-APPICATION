614bb7044168c62ed8808e283f0200fd2116b6fa
vaaluvishnu5146 <vaaluvishnu5146@gmail.com>
2023-12-12 10:25:49 +0530

    Added full screen api functionality

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
-console.log("Linked script file");
+// function requestFullScreen() {
+//   var element = document.body;
+//   // Supports most browsers and their versions.
+//   var requestMethod =
+//     element.requestFullScreen ||
+//     element.webkitRequestFullScreen ||
+//     element.mozRequestFullScreen ||
+//     element.msRequestFullScreen;
+
+//   if (requestMethod) {
+//     // Native full screen.
+//     requestMethod.call(element);
+//   } else if (typeof window.ActiveXObject !== "undefined") {
+//     // Older IE.
+//     var wscript = new ActiveXObject("WScript.Shell");
+//     if (wscript !== null) {
+//       wscript.SendKeys("{F11}");
+//     }
+//   }
+// }
+
+// Make the body go full screen.
+
+// document.addEventListener("keyup", (e) => {
+//   if (e.key == " " || e.code == "Space" || e.keyCode == 32) {
+//     //your code
+//     console.log("Thanks you!");
+//     window.close();
+//   } else {
+//     console.log("Invalid key pressed");
+//   }
+// });
